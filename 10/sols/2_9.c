0a76cb4c5e37876aa0b00622a7e7a36fc8ef6497
Robert Huynh <91921502+ThartWay@users.noreply.github.com>
2025-09-10 21:10:47 +1000

    Delete 10/sols directory

--- 10/sols/2_9.c
+++
@@ -1,9 +0,0 @@
-int count_lowercase(char string[MAX_CHARS]) {
-    int count = 0;
-    for (int i = 0; i < string[i] != '\0'; i++) {
-        if (is_lowercase(string[i])) {
-            count++;
-        }
-    }
-    return count;
-}
--- 10/sols/3_4.c
+++
@@ -1,8 +0,0 @@
-void delete_following_words(char string[MAX_CHARS]) {
-    for (int i = 0; i < string[i] != '\0'; i++) {
-        if (string[i] == ' ') {
-            string[i] = '\0';
-        }
-    }
-    return;
-}
