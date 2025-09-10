4146ab416df0eea24bf48d3e115731cb1ecee0d3
Robert Huynh <91921502+ThartWay@users.noreply.github.com>
2025-09-10 21:19:35 +1000

    wk 2

---
+++ 02/scanf_crash_course.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+
+int main(void) {
+	int a = 0;
+	int b = 0;
+    char c = 'a';
+
+    printf("__________________________PART 1_____________________________\n");
+    scanf(" %d", &a);
+    //* What are the value of (a) if we
+    // pressed 3       =>  
+    // pressed f     =>  
+    // pressed 12.2    => 
+
+    printf("__________________________PART 3_____________________________\n");
+    scanf(" %c", &c);
+    //* What are the value of (c) if we
+    // pressed a       =>   
+    // pressed 1       =>   
+
+    printf("__________________________PART 3_____________________________\n");
+    scanf(" %d %d", &a, &b);
+    //* What are the values of (a) and (b) if we:
+    //*                      a               b
+    // pressed 3,8,9,\n =>   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
